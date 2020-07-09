301ee6fcdab75f2408b929476a1c4e52aa4f74b2
severin122 <severin1992@yahoo.de>
2020-07-09 18:59:32 +0200

    react beach resort application

--- src/contentful.js
+++ src/contentful.js
@@ -1,7 +1,6 @@
 import {createClient} from 'contentful'
 
 export default createClient({
-	space: "asgd9a1bh71j",
-	accessToken:
-		"yuDlzuE18hRzEbTDrW__Sa0T9JZpzeFI98I3fOter7w"
+	space: process.env.REACT_APP_API_SPACE,
+	accessToken: process.env.REACT_APP_ACCESS_TOKEN
 })
